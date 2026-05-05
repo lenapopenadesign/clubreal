69b5d14cf1c7d88f738bcfc02568b131ae042114
Parlamentwatch <parlamentwatch@Mac.fritz.box>
2026-05-05 21:14:12 +0200

    Add Tina backend initialisation files

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,2 +1,5 @@
-export default ()=>({})
-export const client = ()=>({})
+import { createClient } from "tinacms/dist/client";
+import { queries } from "./types";
+export const client = createClient({ cacheDir: '/Users/parlamentwatch/Documents/Repos/club-real/club-real-website/tina/__generated__/.cache/1778007785404', url: 'https://content.tinajs.io/2.3/content/684bbcab-f24c-4aa8-b766-37628385f2fb/github/main', token: '836655d09835177e832f48659556886565f08bcb', queries,  });
+export default client;
+  
